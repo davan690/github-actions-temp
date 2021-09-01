ddc3af70a34f61e837f1ef3cc2607b006c1b6880
Anthony Davidson <anthony.davidson@canberra.edu.au>
2021-09-01 13:43:17 +1000

    new branch new rcode

---
+++ health_act_page.R
@@ -0,0 +1,121 @@
+#https://raw.githubusercontent.com/amrrs/scrape-automation/main/nifty50_scraping.R
+
+#load libs
+library(plyr)
+# library(tidyverse)
+library(rvest)
+# library(janitor)
+# library(leaflet)
+# library(ggmap)
+# library(rvest)
+library(RSelenium)
+# library(tidyverse)
+library(DescTools)
+library(knitr)
+# library(mapview)
+library(arsenal)
+# library(rgdal)
+# library(dplyr)
+
+# saveRDS(url_html, "data/test.rds")
+#load google api
+# gapi <- readLines("c:/bernd/r/covid_canberra/gapi.txt")
+register_google(gapi)
+
+#nse top gainers
+url <- 'https://www.covid19.act.gov.au/'
+
+# extract html 
+url_html <- read_html(url)
+
+#grab from website
+es <- read_html("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations", )
+
+#check update
+ll <- es %>%
+  html_nodes("strong") %>%
+  html_text()
+index <- grep("Page last updated:",ll)
+dummy <- ll[index]
+lup <- dummy
+lu <- substr(strsplit(dummy,"updated:")[[1]][2],2,100)
+lu <- gsub(" ", "_",lu)
+lu <- gsub(":","",lu)
+lu
+
+#check if there was an update....
+ff <- list.files("data/")
+wu <- grep(lu, ff)
+
+#table extraction
+##### scrape covid exposure table from website
+
+rD <- rsDriver(browser="firefox", port=4545L, verbose=FALSE)
+remDr <- rD[["client"]]
+remDr$navigate("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations")
+
+Sys.sleep(5) # give the page time to fully load
+
+#click the archived button
+#arch$clickElement()
+#html <- remDr$getPageSource()[[1]]
+
+html <- remDr$getPageSource()[[1]]
+
+remDr$close()
+rD$server$stop()
+rm(rD)
+gc()
+#necessary to stop the server...
+system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+
+signals <- read_html(html)
+
+
+tbls <- signals %>%
+  html_nodes("table") %>%
+  html_table(fill = TRUE)
+
+tab3 <- data.frame(tbls)
+
+
+#change empty to previous
+tab3$Status <- ifelse(tab3$Status=="New","New","")
+tab3$type <- paste(tab3$Contact, tab3$Status)
+
+###todo check only new sites and not the once we have data from
+#load last.csv
+
+ldata <- read.csv("data/last.csv")
+#check identical entries column Exposure.Location
+ldata$check <- paste(ldata$Exposure.Location, ldata$Street, ldata$Suburb, ldata$Date, ldata$Arrival.Time, ldata$Departure.Time)
+tab3$check <- paste(tab3$Exposure.Location, tab3$Street, tab3$Suburb, tab3$Date, tab3$Arrival.Time, tab3$Departure.Time)
+tab3 <- plyr::join(tab3, ldata[,c("lat","lon","check")],  by="check")
+tab3$check <- NULL
+ldata$check <- NULL
+
+toadd <- which(is.na(tab3$lat))
+
+# new info to add
+
+# #add lat lon
+# #
+# if (length(toadd)>0)
+# {
+#   tt <- tab3[toadd,]
+#   #get coordinates only for those where lat lon is empty
+#   
+#   address <- geocode(paste0(tt$Street,", ", tt$Exposure.Location,", ",tt$Suburb ,", Canberra, Australia"))
+#   
+#   tab3$lat[toadd] <- address$lat
+#   tab3$lon[toadd] <- address$lon
+# }
+
+ #folder path needs to change          
+ write_csv(tab3,paste0('data/',Sys.Date(),'_act_location_run',lu, '.csv'))    
+ 
+ write_csv(tab3, 'data/last_test.csv') 
+
+
+
+       
